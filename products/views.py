6538c35ae24f2090cc837fe424807754961561fe
Eletrinho <cauacustodiopereira@gmail.com>
2026-04-22 19:43:23 -0300

    add serializer for product

--- products/views.py
+++ products/views.py
@@ -2,20 +2,22 @@ from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from drf_spectacular.utils import extend_schema
 from .models import Product
+from .serializers import ProductSerializer
 
 @extend_schema(operation_id="products_list")
 @api_view(['GET'])
 def product_list_view(request):
-    products = Product.objects.all()
-    product_data = [{"id": product.id, "name": product.name, "price": product.price} for product in products]
-    return Response(product_data)
+    serializer = ProductSerializer(data=Product.objects.all(), many=True)
+    if serializer.is_valid():
+        return Response(serializer.data)
+    return Response(serializer.errors, status=400)
 
 @extend_schema(operation_id="product_detail")
 @api_view(['GET'])
 def product_detail_view(request, product_id):
     try:
         product = Product.objects.get(id=product_id)
-        product_data = {"id": product.id, "name": product.name, "price": product.price}
-        return Response(product_data)
+        serializer = ProductSerializer(product)
+        return Response(serializer.data)
     except Product.DoesNotExist:
         return Response({"error": "Product not found."}, status=404)
